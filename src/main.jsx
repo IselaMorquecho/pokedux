4d2761ef4ec4ee4236ea92d38de56eac73194b75
Isela Morquecho <isela.morquecho@patronato.unam.mx>
2024-01-02 16:47:06 -0600

    Agrego custom middlewares

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,18 @@ import App from "./App.jsx";
 import "./index.css";
 import { pokemonsReducer } from "./reducers/pokemons.js";
 import { Provider } from "react-redux";
-import { legacy_createStore as createStore } from "redux";
+import {
+  legacy_createStore as createStore,
+  compose,
+  applyMiddleware,
+} from "redux";
+import { featuring, logger } from "./middlewares/index.js";
 
-const store = createStore(
-  pokemonsReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const composedEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger, featuring)
 );
+const store = createStore(pokemonsReducer, composedEnhancers);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
