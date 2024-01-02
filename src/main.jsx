ef24c051b02b50ab506c547c90d79cf5b1c79887
Isela Morquecho <isela.morquecho@patronato.unam.mx>
2024-01-01 22:08:33 -0600

    agrego devtools extension

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,10 @@ import { pokemonsReducer } from "./reducers/pokemons.js";
 import { Provider } from "react-redux";
 import { legacy_createStore as createStore } from "redux";
 
-const store = createStore(pokemonsReducer);
+const store = createStore(
+  pokemonsReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
